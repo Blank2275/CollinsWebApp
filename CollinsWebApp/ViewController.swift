ed3a4e9a54a514d7eddb5bb59824e0da363c04cd
MIMJA156 <75694661+MIMJA156@users.noreply.github.com>
2022-07-05 15:09:12 -0600

    Added needs help indicator

--- CollinsWebApp/ViewController.swift
+++ CollinsWebApp/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var needsHelpCirlceView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        needsHelpCirlceView.layer.cornerRadius = ((needsHelpCirlceView.bounds.width + needsHelpCirlceView.bounds.height) / 4)
+        needsHelpCirlceView.layer.backgroundColor = CGColor.init(red: 255, green: 0, blue: 0, alpha: 1)
+        
+        colorCirlce()
+        
+        Firestore.firestore().collection("test").document("oof").addSnapshotListener({ documentSnapshot, error in
+            print("run")
+            self.colorCirlce()
+        })
     }
-
-
+    
+    func colorCirlce(){
+        let docRef = Firestore.firestore().collection("test").document("oof")
+        
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                let dataDescription = document.data()!
+                let needsHelp = dataDescription["needsHelp"]!
+                if(needsHelp) as! Bool{
+                    self.needsHelpCirlceView.layer.backgroundColor = CGColor.init(red: 0, green: 255, blue: 0, alpha: 1)
+                }else{
+                    self.needsHelpCirlceView.layer.backgroundColor = CGColor.init(red: 255, green: 0, blue: 0, alpha: 1)
+                }
+            } else {
+                print("Document does not exist")
+            }
+        }
+    }
 }
-
